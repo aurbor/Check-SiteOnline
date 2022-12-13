853c579bd78d6f3ba5e8c19a1e857131a166d970
Evan Wilson <54943302+aurbor@users.noreply.github.com>
2022-12-13 14:43:02 +0000

    Update Check-SiteOnline.ps1

--- Check-SiteOnline.ps1
+++ Check-SiteOnline.ps1
@@ -2,28 +2,11 @@
 
 $url = "https://www.google.com"
 
-## Check if using HTTP or HTTPS
+## Test HTTP Response code based on URL above and return 'True' if connection is successful, and 'False' if the connection is not and log an error.
 
-$protocol = $url.split('://')[0]
-
-If ($protocol -eq "http") {
-    $port = 80
-} ElseIf ($protocol -eq "https") {
-    $port = 443
-} Else {
-    Throw "There's an issue with the URL. Please check that you've included https:// or http:// in the address and try again."
-}
-
-## Test HTTP Response code based on chosen port and return 'True' if connection is successful, and 'False' if the connection is not.
-
-if (-not (Test-NetConnection -ComputerName $hostname -Port $port -WarningAction SilentlyContinue -ErrorAction SilentlyContinue).TcpTestSucceeded) { 
-    Write-Host "There's an issue connecting on the chosen port. Please verify that you're using the correct protocol in the URL (http:// or https://) and try again."
-    $false
+$response = Invoke-WebRequest -Uri $url -UseBasicParsing
+if ($response.StatusCode -eq 200) {
+    return $true
 } else {
-    $response = Invoke-WebRequest -Uri $url -UseBasicParsing
-    if ($response.StatusCode -ne 200) {
-        return $false
-    } else {
-        return $true
-    }
+    return $false
 }
